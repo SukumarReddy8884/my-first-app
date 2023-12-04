527002d059bf21890f99c9c5e63d3e0af6fa1daa
Sukumar <sukumarreddy8884@gmail.com>
2023-12-04 17:00:34 -0600

    Added a service to pull all properties from  a json file

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-property-card',
@@ -7,10 +7,5 @@ import { Component } from "@angular/core";
 })
 
 export class PropertyCardComponent{
-    Property: any={
-        "ID":1,
-        "Type":"House",
-        "Name": "Birla House",
-        "Price":12000
-    }
+    @Input() Property:any;
 }
